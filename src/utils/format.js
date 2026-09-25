172b9d007ecbee80e8c4f4946fd4ce4ea5b03787
Uriel Cisneros <urielcisne96@gmail.com>
2026-09-24 21:07:12 -0600

    se elimina archivos innesesarios

--- src/utils/format.js
+++
@@ -1,9 +0,0 @@
-const formatter = new Intl.NumberFormat('es-MX', {
-  style: 'currency',
-  currency: 'MXN',
-  maximumFractionDigits: 0,
-});
-
-export function formatPrice(value) {
-  return formatter.format(value);
-}
